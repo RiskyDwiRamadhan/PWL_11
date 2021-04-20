a1e7ab60d33961e2c6e700a2e220e21c10df23d2
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-04-20 20:26:36 +0700

    Menambahkan fungsi register pada controller auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+use App\Models\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -8,4 +9,20 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     use ApiResponse;
+
+    public function register(RegisterRequest $request){
+        $validated = $request->validated();
+        $user = User::Create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),            
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return $this->apiSuccess([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
 }
